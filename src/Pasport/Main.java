773253be5289502b34fa87a4782906f8b6e03177
Лидия Дворкина <lideya832@mail.ru>
2022-11-23 21:34:56 +0300

    Задачи написаны, но во многих есть элементы подчеркнутые красным. Не помогу определить причину. Прошу помочь.

--- src/Pasport/Main.java
+++ src/Pasport/Main.java
@@ -1,16 +1,13 @@
 package Pasport;
-
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
-
 public class Main {
-
+    Passport passport = new Passport (1803568947,"Сухов", "Иван", "Васильевич",
+            1982_02_02);
     private final Set<Passport> passports = new HashSet<>();
-
     public void addOrUpdatePassport(Passport passport) {
-        this.passports.add(passport);
-    }
-
+        this.passports.add(passport); }
     public Passport findByNumber(int number) {
         for (Passport passport : passports) {
             if (passport.getNumber() == number) {
--- src/Pasport/Passport.java
+++ src/Pasport/Passport.java
@@ -1,36 +1,27 @@
 package Pasport;
 
+import java.time.LocalDate;
 import java.util.Objects;
 
 public class Passport {
-
     private final int number;
-
     private final String surname;
-
     private final String name;
-
     private final String secondName;
+    private final LocalDate localDate;
 
-    private final int dateOfBirth;
-
-
-    public Passport(int number, String surname, String name, String secondName, int dateOfBirth) {
+    public Passport(int number, String surname, String name, String secondName, LocalDate localDate) {
         this.number = number;
         this.surname = surname;
         this.name = name;
         this.secondName = secondName;
-        this.dateOfBirth = dateOfBirth;
+        this.localDate = localDate;
     }
-        public int getNumber() { return number; }
-
+    public int getNumber() { return number; }
         public String getSurname() {return surname; }
-
         public String getName() {return name; }
-
         public String getLastName() {return secondName; }
-
-        public int getDateOfBirth() { return dateOfBirth; }
+        public LocalDate getLocalDate() {return localDate; }
 
     @Override
     public boolean equals(Object o) {
