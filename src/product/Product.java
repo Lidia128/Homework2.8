6729923cd58862c86642623cbd62bc229ff7a0d5
Лидия Дворкина <lideya832@mail.ru>
2022-11-28 22:13:54 +0300

    Домашнее заданиее по ассоциативным массивом и картам

--- src/product/Product.java
+++ src/product/Product.java
@@ -1,24 +1,17 @@
-package Product;
+package product;
 import java.util.Objects;
 public class Product {
     private final String name;
     private final double price;
-    private final int weight;
-    private boolean cheked;
-    public Product(String name, double price, int weight) {
-        if (name == null || name.isBlank() || price < 0 || weight < 0) {
+    public Product(String name, double price) {
+        if (name == null || name.isBlank() || price < 0) {
             throw new IllegalArgumentException("Заполните карточку товара полностью");
         }
         this.name = name;
         this.price = price;
-        this.weight = weight;
-        this.cheked = false;
     }
     public String getName() {return name;}
     public double getPrice() {return price;}
-    public double getWeight() {return weight; }
-    public void setCheked() {this.cheked = true;}
-    public boolean isCheked() {return cheked; }
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -36,8 +29,7 @@ public class Product {
     }
     @Override
     public String toString() {
-        String checkedString = this.isCheked() ? "Да" : "Нет";
-        return String.format("%s, Цена : %s, Количество : %s, Куплен : %s",
-                this.name, this.price, this.weight, checkedString);
+        return String.format("%s, Цена : %s",
+                this.name, this.price);
     }
 }
